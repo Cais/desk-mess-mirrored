4c781ab42f41bb84c1aa678329dceb7391c0cb0d
Edward Caissie <edward.caissie@gmail.com>
2011-11-28 14:57:08 -0500

    Initial documentation/layout review of 'footer.php'

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-    <div id="bottom"></div> <!-- #bottom -->
-    <div id="bottom-extended">
-      <div id="bottom-container">
-        <p>
-          <?php bns_dynamic_copyright();
-          bns_theme_version(); ?>
-        </p>
-        <?php wp_footer(); ?>
-      </div> <!-- #bottom-container -->
-    </div> <!-- #bottom-extended -->
-  </div> <!-- #mainwrap -->
-  <!-- Desk Mess Mirrored v1.8.6 - http://BuyNowShop.com/themes/desk-mess-mirrored -->
+        <div id="bottom"></div>
+        <div id="bottom-extended">
+            <div id="bottom-container">
+                <p>
+                    <?php bns_dynamic_copyright();
+                    bns_theme_version(); ?>
+                </p>
+                <?php wp_footer(); ?>
+            </div> <!-- #bottom-container -->
+        </div> <!-- #bottom-extended -->
+    </div> <!-- #mainwrap -->
+    <!-- Desk Mess Mirrored v1.9.1 - http://BuyNowShop.com/themes/desk-mess-mirrored -->
 <?php /* Last revised June 9, 2011 v1.8.6 */ ?>
 </body>
 </html>
