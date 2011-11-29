5b31739ffb33aa91dac489c2fbbb609c33b5f990
Edward Caissie <edward.caissie@gmail.com>
2011-11-29 12:07:50 -0500

    Reviewed and correct old changelog entries for spelling and other minor typos

--- comments.php
+++ comments.php
@@ -9,10 +9,18 @@ if ( post_password_required() ) { ?>
 /**
  * Add Comment Classes
  *
+ * Function creates classes to be added to the general comment_class via an
+ * add_filter action.
+ *
  * @package Desk_Mess_Mirrored
  *
  * @param $classes
  * @return array
+ *
+ * @version 1.9.1
+ * Last revised November 29, 2011
+ * Combined user_ID class and microid class generation functions into a single function.
+ * @todo Review user_ID class to be changed to user_capability class, or add user_capability class
  */
 function dmm_add_comment_classes( $classes ) {
         global $comment;
@@ -40,24 +48,25 @@ add_filter( 'comment_class', 'dmm_add_comment_classes' );
 ?>
 
 <div id="comments-main">
-<?php // show the comments
+    <?php
+    /** Show comments */
 	if ( have_comments() ) : ?>
-	  <h4 id="comments"><?php comments_number( __( 'No Comments', 'desk-mess-mirrored' ), __( '1 Comment', 'desk-mess-mirrored' ), __( '% Comments', 'desk-mess-mirrored' ) );?></h4>
-		<ul class="commentlist" id="singlecomments">
-			<?php wp_list_comments( array( 'avatar_size' => 60, 'reply_text' => __( '&raquo; Reply to this Comment &laquo;', 'desk-mess-mirrored' ) ) ); ?>
-		</ul>
-		<div class="navigation">
-			<div class="alignleft"><?php previous_comments_link() ?></div>
-			<div class="alignright"><?php next_comments_link() ?></div>
-		</div>
-	<?php else : // this is displayed if there are no comments so far ?>
-		<?php if ( 'open' == $post->comment_status ) :
-			// If comments are open, but there are no comments.
-		else : 
-			// comments are closed 
-		endif;
-	endif;
-	comment_form();
-?>
+        <h4 id="comments"><?php comments_number( __( 'No Comments', 'desk-mess-mirrored' ), __( '1 Comment', 'desk-mess-mirrored' ), __( '% Comments', 'desk-mess-mirrored' ) );?></h4>
+        <ul class="commentlist" id="singlecomments">
+            <?php wp_list_comments( array( 'avatar_size' => 60, 'reply_text' => __( '&raquo; Reply to this Comment &laquo;', 'desk-mess-mirrored' ) ) ); ?>
+        </ul>
+        <div class="navigation">
+            <div class="alignleft"><?php previous_comments_link() ?></div>
+            <div class="alignright"><?php next_comments_link() ?></div>
+        </div>
+    <?php else : // Display if there are no comments so far
+        /** @var $post string */
+        if ( 'open' == $post->comment_status ) :
+            // If comments are open, but there are no comments.
+        else :
+            // comments are closed
+        endif;
+    endif;
+    comment_form(); ?>
 </div> <!-- #comments-main -->
-<?php /* Last Revision: November 27, 2010 v1.8.2 */ ?>
+<?php /* Last revised November 28, 2011 v1.9.1 */ ?>
