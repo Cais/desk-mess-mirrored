84c29edba3254b08d4fd2f0456c4d1a6834043e5
Edward Caissie <edward.caissie@gmail.com>
2011-12-08 16:21:42 -0500

    Minor updates to 'changelog'

--- author.php
+++ author.php
@@ -32,6 +32,7 @@ $curauth = ( get_query_var( 'author_name ') ) ? get_user_by( 'id', get_query_var
             <div id="author" class="<?php
                     /**
                      * Add class as related to the user role (see 'Role:' drop-down in User options)
+                     * @todo add additional CSS to reflect the new classes being used for authors
                      */
                     if ( user_can( $curauth->ID, 'administrator' ) ) {
                         echo 'administrator';
